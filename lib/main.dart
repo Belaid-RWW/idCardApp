6050b204e08228ac617544a88f8ae5be12bc3564
Fedy Belaid <fedybelaid1902@gmail.com>
2022-01-26 12:58:55 +0100

    Added avatar and user info

--- lib/main.dart
+++ lib/main.dart
@@ -11,11 +11,72 @@ class MyCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[900],
       appBar: AppBar(
         title: Text('My Card!'),
         centerTitle: true,
         backgroundColor: Colors.grey[850],
       ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/zed.jpg'),
+                radius: 40.0,
+              ),
+            ),
+            Divider(
+              height: 90.0,
+              color: Colors.grey[800],
+            ),
+            SizedBox(height: 20.0),
+            Text('Name',
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0,
+            ),),
+            SizedBox(height: 10.0),
+            Text('Fedy Belaid',
+            style: TextStyle(
+              color: Colors.amberAccent,
+              letterSpacing: 2.0,
+              fontSize: 16.0,
+              fontWeight: FontWeight.bold
+            ),),
+            SizedBox(height: 30.0),
+            Text('Age',
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0,
+              ),),
+            SizedBox(height: 10.0),
+            Text('24 years old',
+              style: TextStyle(
+                  color: Colors.amberAccent,
+                  letterSpacing: 2.0,
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.bold
+              ),),
+            SizedBox(height: 30.0),
+            Row(
+              children: <Widget>[
+                Icon(
+                  Icons.email,
+                  color: Colors.grey[400],
+                ),
+                SizedBox(width: 10.0),
+                Text('abc@gmail.com', style: TextStyle(
+                  color: Colors.grey[400],
+                  letterSpacing: 1.0
+                ),)
+              ],
+            )
+          ],
+        ),
+      )
     );
   }
 }
